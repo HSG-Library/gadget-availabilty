b78007ea2bfb4515909bc20ffbfe9cc3e62dfda6
Jonas Furrer <jonas.furrer@gmail.com>
2022-01-12 10:54:52 +0100

    Allow to configure the apiKey via different sources
    - Renaming of the property name necessary

--- src/main/java/ch/unisg/biblio/systemlibrarian/Application.java
+++ src/main/java/ch/unisg/biblio/systemlibrarian/Application.java
@@ -12,9 +12,9 @@ public class Application {
     private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     public static void main(String[] args) {
-        if (System.getProperty("apiKey") == null) {
-            LOG.error("API key not set (-DapiKey=\"<key>\")");
-            System.err.println("API key not set (-DapiKey=\"<key>\")");
+        if (System.getProperty("alma-api.apiKey") == null) {
+            LOG.error("API key not set (-Dalma-api.apiKey=\"<key>\")");
+            System.err.println("API key not set (-Dalma-api.apiKey=\"<key>\")");
             System.exit(1);
         }
         Micronaut.run(Application.class, args);
