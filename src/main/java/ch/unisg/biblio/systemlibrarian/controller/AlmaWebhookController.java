b3930dcffab81da2ed2a8e00618a1cd86c6ffb40
Jonas Furrer <jonas.furrer@gmail.com>
2022-01-19 10:51:36 +0100

    Move webhook logic to own service

--- src/main/java/ch/unisg/biblio/systemlibrarian/controller/AlmaWebhookController.java
+++ src/main/java/ch/unisg/biblio/systemlibrarian/controller/AlmaWebhookController.java
@@ -1,22 +1,12 @@
 package ch.unisg.biblio.systemlibrarian.controller;
 
 import java.lang.invoke.MethodHandles;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 import java.util.Map;
-import java.util.Optional;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import ch.unisg.biblio.systemlibrarian.AlmaClientConfig;
-import ch.unisg.biblio.systemlibrarian.clients.models.AlmaWebhookLoanItem;
-import ch.unisg.biblio.systemlibrarian.services.GatgetProviderService;
-import ch.unisg.biblio.systemlibrarian.services.HMACService;
+import ch.unisg.biblio.systemlibrarian.services.WebhookHandlerService;
 import io.micronaut.context.annotation.ConfigurationProperties;
 import io.micronaut.context.annotation.Requires;
 import io.micronaut.http.HttpRequest;
@@ -35,32 +25,11 @@ public class AlmaWebhookController {
 
 	private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
-	private static final String LOAN_ACTION = "LOAN";
-	private static final String EVENT_LOAN_CREATED = "LOAN_CREATED";
-	private static final String EVENT_LOAN_RETURNED = "LOAN_RETURNED";
-
-	private String algorithm;
-	private String secret;
-
-	private HMACService hmacService;
-	private AlmaClientConfig almaClientConfig;
-	private GatgetProviderService gadgetProviderService;
+	private WebhookHandlerService webhookHandlerService;
 
 	@Inject
-	public AlmaWebhookController(HMACService hmacService,
-			AlmaClientConfig config,
-			GatgetProviderService gadgetProviderService) {
-		this.hmacService = hmacService;
-		this.almaClientConfig = config;
-		this.gadgetProviderService = gadgetProviderService;
-	}
-
-	public void setAlgorithm(String algorithm) {
-		this.algorithm = algorithm;
-	}
-
-	public void setSecret(String secret) {
-		this.secret = secret;
+	public AlmaWebhookController(WebhookHandlerService webhookHandlerService) {
+		this.webhookHandlerService = webhookHandlerService;
 	}
 
 	@Get("/webhook")
@@ -76,68 +45,6 @@ public class AlmaWebhookController {
 		LOG.info("Webhook recieved");
 		String body = request.getBody().orElse("empty");
 		LOG.trace("Signature: '{}' Body: '{}'", signature, body);
-		return processWebhook(body, signature);
-	}
-
-	protected HttpResponse<String> processWebhook(String body, String signature) {
-		boolean webhookValid = checkHMAC(body, signature);
-		if (!webhookValid) {
-			LOG.warn("The recieved webhook was not correctly signed");
-			return HttpResponse.badRequest();
-		}
-		Optional<AlmaWebhookLoanItem> item = convert(body);
-		if (item.isEmpty()) {
-			return HttpResponse.badRequest("No Item");
-		}
-		item.filter(this::filterWebhook)
-				.ifPresent(i -> {
-					boolean available;
-					if (EVENT_LOAN_CREATED.equals(i.getEvent().getValue())) {
-						available = false;
-					} else if (EVENT_LOAN_RETURNED.equals(i.getEvent().getValue())) {
-						available = true;
-					} else {
-						// event other than created/returned -> availabilty stays the same
-						return;
-					}
-					gadgetProviderService.updateAvailability(i.getItemLoan().getItemBarcode(), available);
-				});
-		return HttpResponse.ok();
-	}
-
-	private boolean checkHMAC(String body, String signature) {
-		try {
-			return hmacService.checkSignature(algorithm, body, secret, signature);
-		} catch (InvalidKeyException | NoSuchAlgorithmException e) {
-			LOG.error("Could not validate webhook, checking the signature failed", e);
-			return false;
-		}
-	}
-
-	protected Optional<AlmaWebhookLoanItem> convert(String jsonBody) {
-		ObjectMapper objectMapper = new ObjectMapper()
-				.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-		try {
-			return Optional.of(objectMapper.readValue(jsonBody, AlmaWebhookLoanItem.class));
-		} catch (JsonProcessingException e) {
-			LOG.error("Could not convert webhook request body to object", e);
-			return Optional.empty();
-		}
-	}
-
-	protected boolean filterWebhook(AlmaWebhookLoanItem item) {
-		return isLoanAction(item) && isCorectMmsId(item) && isCorrectHoldingId(item);
-	}
-
-	private boolean isLoanAction(AlmaWebhookLoanItem item) {
-		return LOAN_ACTION.equals(item.getAction());
-	}
-
-	private boolean isCorectMmsId(AlmaWebhookLoanItem item) {
-		return almaClientConfig.getMmsId().equals(item.getItemLoan().getMmsId());
-	}
-
-	private boolean isCorrectHoldingId(AlmaWebhookLoanItem item) {
-		return almaClientConfig.getHoldingId().equals(item.getItemLoan().getHoldingId());
+		return webhookHandlerService.processWebhook(body, signature);
 	}
 }
--- src/test/java/ch/unisg/biblio/systemlibrarian/services/WebhookHandlerServiceTest.java
+++ src/test/java/ch/unisg/biblio/systemlibrarian/services/WebhookHandlerServiceTest.java
@@ -1,19 +1,18 @@
-package ch.unisg.biblio.systemlibrarian.controller;
+package ch.unisg.biblio.systemlibrarian.services;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+
+import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import ch.unisg.biblio.systemlibrarian.clients.AlmaClient;
 import ch.unisg.biblio.systemlibrarian.clients.models.AlmaItemResponse;
-import io.micronaut.http.HttpRequest;
+import ch.unisg.biblio.systemlibrarian.clients.models.AlmaWebhookLoanItem;
 import io.micronaut.http.HttpResponse;
-import io.micronaut.http.MutableHttpRequest;
-import io.micronaut.http.client.HttpClient;
-import io.micronaut.http.client.annotation.Client;
 import io.micronaut.test.annotation.MockBean;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
@@ -21,17 +20,13 @@ import jakarta.inject.Inject;
 @MicronautTest(environments = {
 		"test"
 })
-class AlmaWebhookControllerTest {
+public class WebhookHandlerServiceTest {
 
 	private static final String JSON_BODY = "{\"id\":\"4716072711972883959\",\"action\":\"LOAN\",\"institution\":{\"value\":\"41SLSP_HSG\",\"desc\":\"Universität St.Gallen\"},\"time\":\"2022-01-12T08:08:56.198Z\",\"event\":{\"value\":\"LOAN_CREATED\",\"desc\":\"Loan created\"},\"item_loan\":{\"loan_id\":\"2410068350005506\",\"circ_desk\":{\"value\":\"DEFAULT_CIRC_DESK\",\"desc\":\"Circulation Desk\"},\"return_circ_desk\":{\"value\":\"\",\"desc\":null,\"link\":null},\"library\":{\"value\":\"HSG\",\"desc\":\"HSG\"},\"user_id\":\"868256628995@eduid.ch\",\"item_barcode\":\"HM00007496\",\"due_date\":\"2022-02-09T22:00:00Z\",\"loan_status\":\"ACTIVE\",\"loan_date\":\"2022-01-12T08:08:54.624Z\",\"returned_by\":{\"value\":\"\",\"link\":null},\"process_status\":\"NORMAL\",\"mms_id\":\"992149060105506\",\"holding_id\":\"2248954680005506\",\"item_id\":\"2348954660005506\",\"title\":\"Egalité les garanties de la Constitution fédérale du 18 avril 1999 Etienne Grisel\",\"author\":\"Grisel, Etienne\",\"description\":null,\"publication_year\":\"2000\",\"location_code\":{\"value\":\"OG\",\"name\":\"Upper Floor\"},\"item_policy\":{\"value\":\"01\",\"description\":\"01 Loan 28 days\"},\"call_number\":\"PL 440 G869\",\"request_id\":{\"value\":\"2406591220005506\",\"link\":\"/almaws/v1/users/868256628995@eduid.ch/requests/2406591220005506\"},\"renewable\":true,\"last_renew_status\":{\"value\":\"\",\"desc\":\"\"}},\"bib\":null,\"holding\":null,\"item\":null,\"portfolio\":null,\"representation\":null}";
 	private static final String SIGNATURE = "t7Dj0H8qWqjRR54hYkznOFAGkhreUWyaHcvZ4Yvb0Ac=";
 
 	@Inject
-	AlmaWebhookController almaWebhookController;
-
-	@Inject
-    @Client("/")
-	HttpClient client;
+	WebhookHandlerService webhookHandlerService;
 
 	@MockBean(AlmaClient.class)
 	AlmaClient almaClient() {
@@ -42,21 +37,18 @@ class AlmaWebhookControllerTest {
 	}
 
 	@Test
-	public void testWebhookChallange() {
-		MutableHttpRequest<String> webhookChallengeGetRequest = HttpRequest
-				.GET("/alma/webhook?challenge=schwalbenweih");
-		String response = client.toBlocking().retrieve(webhookChallengeGetRequest, String.class);
-		System.out.println(response);
-		Assertions.assertEquals("{\"challenge\":\"schwalbenweih\"}", response);
+	void testProcessWebhook() {
+		var response = webhookHandlerService.processWebhook(JSON_BODY, SIGNATURE);
+		Assertions.assertEquals(HttpResponse.ok().code(), response.code());
 	}
 
 	@Test
-	public void testRecieveWebhook() {
-		MutableHttpRequest<String> webhookPostRequest = HttpRequest
-		.POST("/alma/webhook", JSON_BODY)
-		.header("X-Exl-Signature", SIGNATURE);
-		HttpResponse<String> response = client.toBlocking().exchange(webhookPostRequest, String.class);
-		System.out.println(response);
-		Assertions.assertEquals(HttpResponse.ok().status(), response.status());
+	void testConvert() {
+		Optional<AlmaWebhookLoanItem> itemOptional = webhookHandlerService.convert(JSON_BODY);
+		Assertions.assertTrue(itemOptional.isPresent());
+		var item = itemOptional.get();
+		Assertions.assertEquals("4716072711972883959", item.getId());
+		Assertions.assertEquals("LOAN_CREATED", item.getEvent().getValue());
+		Assertions.assertEquals("HM00007496", item.getItemLoan().getItemBarcode());
 	}
 }
