2d7727a3eaf7caa9fae63b2e092b8272a548103e
Jonas Furrer <jonas.furrer@gmail.com>
2022-02-09 07:43:43 +0100

    Re-Initialize gadgets 1h over opening hours

--- src/main/java/ch/unisg/biblio/systemlibrarian/services/GadgetProviderService.java
+++ src/main/java/ch/unisg/biblio/systemlibrarian/services/GadgetProviderService.java
@@ -42,9 +42,9 @@ public class GadgetProviderService implements ApplicationEventListener<ServerSta
 	/**
 	 * Re-initialize gadgets (call Alma API).
 	 * 
-	 * Scheduled every 30min from 07:00 to * 21:00.
+	 * Scheduled every 30min from 07:00 to 20:00.
 	 */
-	@Scheduled(cron = "0 */30 7-21 * * *")
+	@Scheduled(cron = "0 */30 7-20 * * *")
 	public void reInitGadgets() {
 		LOG.info("Scheduled: call initGadgets()");
 		initGadgets();
