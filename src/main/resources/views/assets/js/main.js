ec3f4b717ade7372db70a57deb4af0105041a1c3
Jonas Furrer <jonas.furrer@gmail.com>
2022-06-29 13:36:46 +0200

    Visually hide h1 title

--- src/main/resources/views/assets/js/main.js
+++ src/main/resources/views/assets/js/main.js
@@ -1,123 +1,121 @@
-document.addEventListener("DOMContentLoaded", function () {
-	gadgets.init();
-});
+document.addEventListener('DOMContentLoaded', function () {
+	gadgets.init()
+})
 
 gadgets = {
 	init: function () {
-		console.log("init gadgets");
-		this.loadGadgets();
-		this.registerSearchToggle();
-		this.registerDescriptionToggle();
+		console.log('init gadgets')
+		this.loadGadgets()
+		this.registerSearchToggle()
+		this.registerDescriptionToggle()
 	},
 
 	loadGadgets: function () {
-		util.get("/gadgets/all", this.onLoadGadgets, this.onLoadGadgetsError);
+		util.get('/gadgets/all', this.onLoadGadgets, this.onLoadGadgetsError)
 	},
 
 	onLoadGadgets: function () {
 		if (this.status >= 200 && this.status < 400) {
-			const list = util.byClass('.js-items').item(0);
-			const data = JSON.parse(this.response);
-			let itemsMarkup = "";
-			let searchIndex = [];
-			let totalItems = 0;
-			let totalAvailable = 0;
+			const list = util.byClass('.js-items').item(0)
+			const data = JSON.parse(this.response)
+			let itemsMarkup = ''
+			let searchIndex = []
+			let totalItems = 0
+			let totalAvailable = 0
 			data.forEach((gadget, idx) => {
 				itemsMarkup += item.getMarkup(
 					gadget.volume,
 					idx,
 					gadget.callno,
 					gadget.img_id,
-					(gadget.note || ""),
+					(gadget.note || ''),
 					gadget.available,
 					gadget.total,
 					gadget.details,
-				);
-				totalAvailable += gadget.available;
-				totalItems += gadget.total;
+				)
+				totalAvailable += gadget.available
+				totalItems += gadget.total
 				searchIndex.push(
 					{
 						id: gadget.volume,
-						txt: gadget.callno.toLowerCase() + " " + (gadget.note || "").toLowerCase()
+						txt: gadget.callno.toLowerCase() + ' ' + (gadget.note || '').toLowerCase()
 					}
-				);
-			});
-			list.innerHTML = itemsMarkup;
-			gadgets.initSearch(searchIndex);
-			const title = util.byId("js-title");
-			title.innerHTML = title.innerHTML + " (" + totalAvailable + "&nbsp;&#47;&nbsp;" + totalItems + ")";
+				)
+			})
+			list.innerHTML = itemsMarkup
+			gadgets.initSearch(searchIndex)
 		} else {
-			gadgets.onLoadGadgetsError();
+			gadgets.onLoadGadgetsError()
 		}
-		util.hideLoader();
+		util.hideLoader()
 	},
 
 	onLoadGadgetsError: function () {
-		console.error("Could not load gadgets");
-		util.hideLoader();
-		util.byId("js-error").classList.remove('hide');
+		console.error('Could not load gadgets')
+		util.hideLoader()
+		util.byId('js-error').classList.remove('hide')
 	},
 
 	registerSearchToggle: function () {
-		const searchButton = util.byId("js-search-button");
-		searchButton.addEventListener("click", function () {
-			const input = util.byId("js-search-input");
-			input.classList.toggle("active");
-			if (input.classList.contains("active")) {
-				input.focus();
+		const searchButton = util.byId('js-search-button')
+		searchButton.addEventListener('click', function () {
+			const input = util.byId('js-search-input')
+			input.classList.toggle('active')
+			if (input.classList.contains('active')) {
+				input.focus()
 			} else {
-				input.blur();
+				input.blur()
 			}
-		});
+		})
 	},
 
 	registerDescriptionToggle: function () {
-		document.addEventListener("click", event => {
-			if (event.target.closest(".tile")) {
-				const tile = event.target.closest(".tile");
+		document.addEventListener('click', event => {
+			if (event.target.closest('.tile')) {
+				const tile = event.target.closest('.tile')
 				if (event.altKey) {
-					const details = tile.querySelectorAll(".js-details");
-					details.forEach((el) => { el.classList.toggle("hide"); })
+					const details = tile.querySelectorAll('.js-details')
+					details.forEach((el) => { el.classList.toggle('hide') })
 				} else {
-					const desc = tile.querySelectorAll(".js-description");
-					desc.forEach((el) => { el.classList.toggle("active"); })
+					const desc = tile.querySelectorAll('.js-description')
+					desc.forEach((el) => { el.classList.toggle('active') })
 				}
 			}
-		});
+		})
 	},
 
 	initSearch: function (searchIndex) {
-		const input = util.byId("js-search-input");
-		input.addEventListener("keyup", () => {
-			const term = input.value.toLowerCase();
+		const input = util.byId('js-search-input')
+		input.addEventListener('keyup', () => {
+			const term = input.value.toLowerCase()
 			if (term.length >= 1) {
-				gadgets.search(term, searchIndex);
+				gadgets.search(term, searchIndex)
 			} else {
-				util.byClass('.js-tile').forEach((tile) => tile.classList.remove("hide"));
+				util.byClass('.js-tile').forEach((tile) => tile.classList.remove('hide'))
 			}
-		});
+		})
 	},
 
 	search: function (term, searchIndex) {
 		util.byClass('.js-tile').forEach((tile) => {
-			tile.classList.remove("initial");
-			tile.classList.add("hide");
-		});
+			tile.classList.remove('initial')
+			tile.classList.add('hide')
+		})
 		searchIndex.forEach((entry) => {
 			if (entry.txt.indexOf(term) >= 0) {
-				util.byId(entry.id).classList.remove("hide");
+				util.byId(entry.id).classList.remove('hide')
 			}
-		});
+		})
 	}
-};
+}
 
 item = {
 	getMarkup: function (id, delay, title, img, description, available, total, details) {
-		let availableClass = "available";
+		let availableClass = 'available'
 		if (available == 0) {
-			availableClass = "notAvailable";
+			availableClass = 'notAvailable'
 		}
-		const delayMs = Math.floor((delay / 3) * 100);
+		const delayMs = Math.floor((delay / 3) * 100)
 		const markup = '<li class="tile initial js-tile ' + availableClass + '" id="' + id + '" style="animation-delay: ' + delayMs + 'ms; ">' +
 			'<div class="info js-info"><h2>' + title + '</h2></div>' +
 			'<div class="container" style="background-image: url(assets/img/' + img + '), url(assets/img/unavailable.jpg);;">' +
@@ -126,29 +124,29 @@ item = {
 			'<p class="availability">' + available + ' / ' + total + '</p>' +
 			'</div>' +
 			'</li >'
-		return markup;
+		return markup
 	}
 }
 
 
 util = {
 	byId: function (id) {
-		return document.getElementById(id);
+		return document.getElementById(id)
 	},
 
 	byClass: function (className) {
-		return document.querySelectorAll(className);
+		return document.querySelectorAll(className)
 	},
 
 	hideLoader: function () {
-		util.byId('js-loader').classList.add('hide');
+		util.byId('js-loader').classList.add('hide')
 	},
 
 	get: function (uri, success, error) {
-		const request = new XMLHttpRequest();
-		request.open("GET", uri, true);
-		request.onload = success;
-		request.onerrorm = error;
-		request.send();
+		const request = new XMLHttpRequest()
+		request.open('GET', uri, true)
+		request.onload = success
+		request.onerrorm = error
+		request.send()
 	}
-};
+}
